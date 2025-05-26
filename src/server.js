6c08c72681c6d39d8433ab9330668027c1114302
phoebe7075 <phoebe7075@gmail.com>
2025-05-26 11:27:39 +0900

    3-3까지 강의내용

---
+++ src/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+const PORT = 4000;
+const app = express();
+
+const handleHome = (req, res) => {
+    //console.log(req);
+    //return res.end(); //request를 종료시키는 방법 중 하나.
+    return res.send("It is end");
+};
+
+app.get("/", handleHome);
+
+
+const handleListening = () => 
+    console.log(`Server Listening on port http://localhost:${PORT}.`);
+
+app.listen(PORT, handleListening);
