b2da3b56af3102ce6b589e3bf38900c79e5742ee
phoebe7075 <phoebe7075@gmail.com>
2025-05-26 17:02:33 +0900

    4.1까지. readme 추가 및 일부 router 추가 관련

--- src/server.js
+++ src/server.js
@@ -6,30 +6,30 @@ const PORT = 4000;
 const app = express();
 const logger = morgan("dev");
 
-const handleHome = (req, res) => { // 이것은 middlewares, controller임.
-    //console.log(req);
-    //return res.end(); //request를 종료시키는 방법 중 하나.
-    return res.send("<h1>It is end</h1>"); //이러한 방식으로 html, json 등을 respond 할 수 있다.
-};
-
-
-
-const handleLogin = (req, res) => {
-    return res.send("Login success.");
-};
-
- 
-//use()는 global middleware = 어디서 접속하든 적용되는 middleware를 적용하는 매서드. 순서 중요
-// 모든 get보다 앞서있으면 무조건 적용되고, 맨 아래에 있다면 get 등으로 등록하지 않은 주소에 대해서만 적용.
-// app.use(logger);
-// app.use(privateMiddleware); 
-
 app.use(logger);
 
-//app.get("/", handleHome); // ~3.4
-//app.get("/", gossipMiddleware, handleHome); // ~3.5
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+const globalRouter = express.Router();
+
+const handleHome = (req, res) => res.send("Home");
+
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+
+const handleEditUser = (req, res) => res.send("Edit User");
+
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
 
 
 const handleListening = () => 
