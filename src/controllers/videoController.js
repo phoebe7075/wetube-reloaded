3f937659e1020a576fc879037703108104972b8d
phoebe7075 <phoebe7075@gmail.com>
2025-05-28 16:46:50 +0900

    6.0 object로 psedo db화 하여 테스트

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,38 +1,41 @@
+let videos = [
+    {
+        title: "First Video",
+        rating:5,
+        comments:2,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id: 1,
+    },
+    {
+        title: "Second Video",
+        rating:4.5,
+        comments:1,
+        createdAt: "1 minutes ago",
+        views: 51,
+        id: 2,
+    },
+    {
+        title: "Third Video",
+        rating:5,
+        comments:0,
+        createdAt: "0 minutes ago",
+        views: 45,
+        id: 3,
+    }
+];
 
 export const trending = (req, res) => {
-    const videos = [
-        {
-            title: "First Video",
-            rating:5,
-            comments:2,
-            createdAt: "2 minutes ago",
-            views: 59,
-            id: 1,
-        },
-        {
-            title: "Second Video",
-            rating:4.5,
-            comments:1,
-            createdAt: "1 minutes ago",
-            views: 51,
-            id: 2,
-        },
-        {
-            title: "Third Video",
-            rating:5,
-            comments:0,
-            createdAt: "0 minutes ago",
-            views: 45,
-            id: 3,
-        }
-    ];
+    
     return res.render("home", {pageTitle : "Home", videos});
  } //pug 파일을 렌더하게 함. 5.1
 export const see = (req, res) => {
-    return res.render("watch", {pageTitle : "Watch"});
+    const { id } = req.params;
+    const video = videos[id-1]
+    return res.render("watch", {pageTitle : `Watch ${video.title}`, video});
 };
 export const edit = (req, res) => {
-    return res.render("edit", {pageTitle : "Edit"});
+    return res.render("edit", {pageTitle : "Edit", videos});
 };
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("upload");
