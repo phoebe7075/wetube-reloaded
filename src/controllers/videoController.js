b2330e09028a3df9b52d2b770962d1765112652e
phoebe7075 <phoebe7075@gmail.com>
2025-05-29 17:50:43 +0900

    promise를 통한 db에서 쿼리 후 데이터 처리 방법 등

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,50 +1,31 @@
-let videos = [
-    {
-        title: "First Video",
-        rating:5,
-        comments:2,
-        createdAt: "2 minutes ago",
-        views: 1,
-        id: 1,
-    },
-    {
-        title: "Second Video",
-        rating:4.5,
-        comments:1,
-        createdAt: "1 minutes ago",
-        views: 51,
-        id: 2,
-    },
-    {
-        title: "Third Video",
-        rating:5,
-        comments:0,
-        createdAt: "0 minutes ago",
-        views: 45,
-        id: 3,
-    }
-];
+import Video from "../models/Video";
 
-export const trending = (req, res) => {
+
+export const home = async (req, res) => {
+    try {
+        const videos = await Video.find({}); //db에서 결과를 받을때까지 대기
+        return res.render("home", {pageTitle : "Home", videos});
+    } catch {
+        return res.render("server-error");
+    }
     
-    return res.render("home", {pageTitle : "Home", videos});
- } //pug 파일을 렌더하게 함. 5.1
+ }
 export const watch = (req, res) => {
     const { id } = req.params;
-    const video = videos[id-1]
-    return res.render("watch", {pageTitle : `Watch ${video.title}`, video});
+    
+    return res.render("watch", {pageTitle : `Watch `});
 };
 export const getEdit = (req, res) => {
     const { id } = req.params;
-    const video = videos[id-1]
-    return res.render("edit", {pageTitle : `Editing: ${video.title}`, video});
+    
+    return res.render("edit", {pageTitle : `Editing: `});
 };
 
 export const postEdit = (req, res) => {
     const { id } = req.params;
     const { title } = req.body
     
-    videos[id-1].title = title;
+    
 
     return res.redirect(`/videos/${id}`)
 }
@@ -57,16 +38,7 @@ export const postUpload = (req, res) => {
     // here we will add a video to the videos array.
     const { title } = req.body
 
-    const videoItem = {
-        title: title,
-        rating: 0,
-        comments:0,
-        createdAt: "0 minutes ago",
-        views: Math.floor(Math.random() * 100),
-        id: videos.length+1,
-    }
-
-    videos.push(videoItem)
+    
 
     return res.redirect("/")
 }
