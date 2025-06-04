102973dbe78046837f479088a7816f2df70da4ef
phoebe7075 <phoebe7075@gmail.com>
2025-06-04 14:27:50 +0900

    6.27, regexp를 통해 regx 조건으로 검색하여 video 찾게 하는 기능 구현

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -77,11 +77,16 @@ export const deleteVideo = async (req, res) => {
     return res.redirect("/");
 }
 
-export const search = (req, res) => {
+export const search = async (req, res) => {
     const {keyword} = req.query;
+    let videos = [];
     if(keyword) {
-        console.log("should search for ", keyword);
+        videos = await Video.find({
+            title: {
+                $regex: new RegExp(keyword, "i")
+            },
+        })
     }
     
-    return res.render("search", {pageTitle:"Search"});
+    return res.render("search", {pageTitle:"Search", videos});
 }
