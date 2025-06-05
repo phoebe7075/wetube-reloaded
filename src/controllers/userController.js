e6799c3334fe984feb3b715f6e08f53a95fc1c99
phoebe7075 <phoebe7075@gmail.com>
2025-06-05 10:11:28 +0900

    login username, password 비교 및 로그인 성공실패 로직 구현

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User"
+import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => res.render("join", {pageTitle: "Join"});
 
@@ -21,16 +22,47 @@ export const postJoin = async (req, res) => {
     }
 
     
-
-    await User.create({
-        name, username, email, password, location
-    });
-    console.log("user join success!");
-    return res.redirect("/login");
+    try {
+        await User.create({
+            name, username, email, password, location
+        });
+        console.log("user join success!");
+        return res.redirect("/login");
+    } catch (error) {
+        return res.status(400).render("join", {
+            pageTitle: "Join",
+            errMsg: error._message,
+        })
+    }
+    
 }
+export const getLogin = (req, res) => res.render("login", {pageTitle:"Login"});
+
+export const postLogin = async (req, res) => {
+    const {username, password} = req.body;
+
+    const user = await User.findOne({username});
+
+    if(!user) {
+        return res.status(400).render("login", {
+            pageTitle:"Login", 
+            errMsg:"An account with this username does not exists."
+        });
+    }
+
+    console.log(user.password);
+    if(!bcrypt.compare(password, user.password)) {
+        return res.status(400).render("login", {
+            pageTitle:"Login",
+            errMsg:"Password not correct. Please Retry one more.",
+        });
+    }
+
+    res.end();
+};
 
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
-export const login = (req, res) => res.send("login");
+
 export const logout = (req, res) => res.send("logout");
 export const see = (req, res) => res.send("See User");
--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { getJoin, login, postJoin } from "../controllers/userController";
+import { getJoin, getLogin, postJoin, postLogin } from "../controllers/userController";
 import { search, home } from "../controllers/videoController";
 
 const rootRouter = express.Router();
 
 rootRouter.get("/", home);
 rootRouter.route("/join").get(getJoin).post(postJoin);
-rootRouter.get("/login", login);
+rootRouter.route("/login").get(getLogin).post(postLogin);
 rootRouter.get("/search", search);
 
 export default rootRouter; //특정 변수만 export 하기 위해 사용,
