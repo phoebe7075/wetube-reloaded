71ba4dfb86513f4f6feecac7b8c81ae4504f2fef
phoebe7075 <phoebe7075@gmail.com>
2025-06-10 10:25:44 +0900

    7.17 github OAuth 기능구현 진행중: router로 url을 옵션 형태로 구현, 인코딩하여 변수형태로 관리하게 구현 등

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -62,6 +62,18 @@ export const postLogin = async (req, res) => {
     return res.redirect("/");
 };
 
+export const startGithubLogin = (req, res) => {
+    const baseUrl = "https://github.com/login/oauth/authorize";
+    const config = {
+        client_id:"Ov23li0a3szUVgOszG0E",
+        allow_signup:false,
+        scope:"read:user user:email",
+    }
+    const params = new URLSearchParams(config).toString();
+    const finalUrl = `${baseUrl}?${params}`;
+    return res.redirect(finalUrl);
+}
+
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
 
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { edit, remove, see, logout } from "../controllers/userController";
+import { edit, remove, see, logout, startGithubLogin } from "../controllers/userController";
 
 
 const userRouter = express.Router();
@@ -7,6 +7,7 @@ const userRouter = express.Router();
 userRouter.get("/edit", edit);
 userRouter.get("/remove", remove);
 userRouter.get("/logout", logout);
+userRouter.route("/github/start").get(startGithubLogin);
 userRouter.get("/:id(\\d+)", see);
 
 
