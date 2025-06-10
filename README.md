bd609f792d0523eb5068050ea53f71ad4d94b1d8
phoebe7075 <phoebe7075@gmail.com>
2025-06-10 11:28:49 +0900

    7.19 github에 node-fetch를 통해 fetch하여 토큰을 받아오고 이걸 이용해 github에 토큰을 fetch하여 유저 정보를 가져오는 기능까지 구현(email은 실패)

--- README.md
+++ README.md
@@ -101,3 +101,4 @@ const validateNumericId = (req, res, next) => {
 - express-session: "1.18.1"
 - connect-mongo: "5.1.0"
 - dotenv: "16.5.0" (env파일을 읽어서 process.env에서 사용 가능하게 해줌)
+- node-fetch: "3.3.2" (nodejs에서 fetch 기능 사용가능하게 해줌)
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { edit, remove, see, logout, startGithubLogin } from "../controllers/userController";
+import { edit, remove, see, logout, startGithubLogin, finishGithubLogin } from "../controllers/userController";
 
 
 const userRouter = express.Router();
@@ -8,6 +8,7 @@ userRouter.get("/edit", edit);
 userRouter.get("/remove", remove);
 userRouter.get("/logout", logout);
 userRouter.route("/github/start").get(startGithubLogin);
+userRouter.route("/github/finish").get(finishGithubLogin);
 userRouter.get("/:id(\\d+)", see);
 
 
