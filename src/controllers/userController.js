3456b4ebbaa30bb3530e45e207ce2dd3e53bd7ce
phoebe7075 <phoebe7075@gmail.com>
2025-06-11 15:10:13 +0900

    7.22 코드 불필요한 부분 수정

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -138,13 +138,11 @@ export const finishGithubLogin = async (req, res) => {
     }
 
 };
-
-export const edit = (req, res) => res.send("Edit User");
-export const remove = (req, res) => res.send("Remove User");
-
 export const logout = (req, res) => {
     req.session.destroy();
     return res.redirect("/");
 };
-export const see = (req, res) => res.send("See User");
 
+
+export const edit = (req, res) => res.send("Edit User");
+export const see = (req, res) => res.send("See User");
