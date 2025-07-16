12ad78cdb82f48dadb8f21701426b23179dc124f
phoebe7075 <phoebe7075@gmail.com>
2025-07-16 09:55:17 +0900

    9.5 minicssextractPlugin으로 css 자동 적용

--- src/server.js
+++ src/server.js
@@ -33,7 +33,7 @@ app.use(localsMiddleware);
 app.use(flashMiddleware);
 
 app.use("/uploads", express.static("uploads"));
-app.use("/static", express.static("assets"))
+app.use("/static", express.static("assets"));
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
