12094abfda6471792e6a9c6b1b4a551c17e5433d
phoebe7075 <phoebe7075@gmail.com>
2025-09-02 16:46:18 +0900

    13.2 MediaRecorder를 통해 녹화 및 녹화된 비디오 Blob 받을 수 있게 수정, 녹화기능 진행중

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,8 +1,35 @@
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
-const handleStart = async () => {
-    const stream = await navigator.mediaDevices.getUserMedia({
+let stream;
+
+const handleStop = () => {
+    startBtn.innerText = "Start Recording";
+    startBtn.removeEventListener("click", handleStop);
+    startBtn.addEventListener("click", handleStart);
+}
+
+const handleStart = () => {
+    startBtn.innerText = "Stop Recording";
+    startBtn.removeEventListener("click", handleStart);
+    startBtn.addEventListener("click", handleStop);
+
+    const recorder = new MediaRecorder(stream);
+    recorder.ondataavailable = (e) => {
+        console.log("recorder");
+        console.log(e);
+        console.log(e.data);
+    }
+    recorder.start();
+
+    setTimeout(() => {
+        recorder.stop();
+        handleStop();
+    }, 10000);
+}
+
+const init = async () => {
+    stream = await navigator.mediaDevices.getUserMedia({
         audio: false, 
         video: {
             width: 200,
@@ -13,5 +40,7 @@ const handleStart = async () => {
     video.srcObject = stream;
     video.play();
 }
+
+init();
 
 startBtn.addEventListener("click", handleStart);
