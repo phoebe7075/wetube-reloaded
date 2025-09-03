199f1a228721c279d514489f00d37b2da9ba0085
phoebe7075 <phoebe7075@gmail.com>
2025-09-03 09:19:02 +0900

    14.1 ffmpeg를 이용한 비디오 컨버트. 버전 업데이트로 작동하지 않는 부분 댓글 참고해서 수정했음

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,3 +1,6 @@
+import { createFFmpeg, FFmpeg } from '@ffmpeg/ffmpeg';
+import { fetchFile, toBlobURL } from '@ffmpeg/util'
+
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
@@ -5,7 +8,24 @@ let stream;
 let recorder;
 let videoFile;
 
-const handleDownload = () => {
+const handleDownload = async () => {
+    const ffmpeg = new FFmpeg();
+    await ffmpeg.load({
+    corePath: '/node_modules/@ffmpeg/core/dist/esm/ffmpeg-core.js',
+    });
+    ffmpeg.on('log', ({message})=>console.log(message));
+
+
+    ffmpeg.writeFile("recording.webm", await fetchFile(videoFile)); //브라우저 상의 ffmpeg 가상 환경에서 파일 생성
+    
+    await ffmpeg.exec(["-i", "recording.webm", "-r", "60", "output.mp4"]); // webm -> mp4 컨버트
+
+    const data = await ffmpeg.readFile('output.mp4');
+    const blob = new Blob([data.buffer], { type: "video/mp4" });
+    const url = URL.createObjectURL(blob);
+    videoFile = url;
+
+
     const a = document.createElement("a");
     a.href = videoFile;
     a.download = "MyRecording.webm";
