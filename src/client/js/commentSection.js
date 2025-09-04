767ba6fc269594c55aee6a39471b23a9025915cc
phoebe7075 <phoebe7075@gmail.com>
2025-09-04 15:50:33 +0900

    16.2 comment box 실제 추가 및 commentSection.js에서 버튼 클릭시 데이터 처리 관련 구현. 아직 전부 구현 안됨

---
+++ src/client/js/commentSection.js
@@ -0,0 +1,13 @@
+const form = document.getElementById("commentForm");
+const textarea = form.querySelector("textarea");
+const btn = form.querySelector("button");
+
+const videoContainer = document.getElementById("videoContainer");
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+    const text = textarea.value;
+    const video = videoContainer.dataset.id;
+}
+
+btn.addEventListener("submit", handleSubmit);
